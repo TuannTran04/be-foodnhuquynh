d6d621f168410d176b70258f62c8acb6c848e2cc
TuannTran04 <skttfaker25@gmail.com>
2023-05-05 17:08:37 +0700

    first

--- config/connectDB.js
+++ config/connectDB.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   database: process.env.MYSQL_DB,
-  password: process.env.MYSQL_PASSWORD,
+  //   password: process.env.MYSQL_PASSWORD,
   port: process.env.MYSQL_PORT,
 });
 // const pool = mysql.createPool({
