51dee6403f91b062fe25b6e339d7dcc0218196de
TuannTran04 <skttfaker25@gmail.com>
2023-05-05 17:02:17 +0700

    first

---
+++ config/connectDB.js
@@ -0,0 +1,20 @@
+// get the client
+// import mysql from 'mysql2'
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DB,
+  //   password: process.env.MYSQL_PASSWORD,
+  port: process.env.MYSQL_PORT,
+});
+// const pool = mysql.createPool({
+// 	host: "buudzxhro1efskecwqu7-mysql.services.clever-cloud.com",
+// 	user: "u9vuc6qosofss30z",
+// 	database: "buudzxhro1efskecwqu7",
+// 	password: "XUO8TmEEXfeyEMo7AeL7",
+// 	port: 3306
+// });
+
+module.exports = pool;
---
+++ index.js
@@ -0,0 +1,45 @@
+// import express from "express";
+// import bodyParser from "body-parser"; // lay cac tham so tu client
+// import cors from 'cors';
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+// import APIRoute from './route/api'
+const APIRoute = require("./route/api.js");
+
+require("dotenv").config();
+
+let app = express();
+// app.use(cors())
+app.use(cors());
+app.use(function (req, res, next) {
+  // cho phép URL_REACT nào đc phép gọi đến api
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", process.env.URL_REACT);
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type,text/plain"
+  );
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+APIRoute(app);
+
+// connectDB();
+let port = process.env.PORT || 6969;
+// port === undefined => gan port = 6969
+app.listen(port, () => {
+  //callback
+  console.log("backend nodejs is running in the port: ", port);
+});
